678a13090f609ca05cb1ffbe46d1801394131653
chuchangk <cailechuchangk@gmail.com>
2012-06-12 14:35:29 +0800

    将 String Process 换成  int pid

--- OSProject/src/com/binary/os/device/Device.java
+++ OSProject/src/com/binary/os/device/Device.java
@@ -4,26 +4,26 @@ import java.util.LinkedList;
 
 class deviceProcessControl //设备进程控制类
 {
-	public static void addListItem(int deviceType, String process){
-		DeviceGlobalVar.devWaitList[deviceType].addLast(process);
+	public static void addListItem(int deviceType, int pid){
+		DeviceGlobalVar.devWaitList[deviceType].addLast(pid);
     }
     
-    public static void delListItem(int deviceType, String process){
-    	DeviceGlobalVar.devWaitList[deviceType].remove(process);
+    public static void delListItem(int deviceType, int pid){
+    	DeviceGlobalVar.devWaitList[deviceType].remove(pid);
     }
     
-	public static boolean allocDevToProcess(int deviceType, String process){
+	public static boolean allocDevToProcess(int deviceType, int pid){
 		int i;
 		for(i=0; i < DeviceGlobalVar.totalDev[deviceType] && !DeviceGlobalVar.devAvail[deviceType][i]; i++);
 		if(i == DeviceGlobalVar.totalDev[deviceType])
 			return false;
-		DeviceGlobalVar.devCurrProc[deviceType][i] = process;
+		DeviceGlobalVar.devCurrPid[deviceType][i] = pid;
 		DeviceGlobalVar.devAvail[deviceType][i] = false;
 		return true;
 	}
 	
 	public static void reclaimDev(int deviceType, int deviceId){   //回收设备后，要注意唤醒等待设备的进程。
 		DeviceGlobalVar.devAvail[deviceType][deviceId] = true;
-		DeviceGlobalVar.devCurrProc[deviceType][deviceId] = null;
+		DeviceGlobalVar.devCurrPid[deviceType][deviceId] = -1;
 	}
 }
--- OSProject/src/com/binary/os/device/DeviceGlobalVar.java
+++ OSProject/src/com/binary/os/device/DeviceGlobalVar.java
@@ -9,5 +9,5 @@ public class DeviceGlobalVar {
 	public static final int totalDev[] = {3, 2, 1};
 	public static boolean devAvail[][] = {{true,true,true}, {true,true}, {true}};
 	public static LinkedList devWaitList[] = new LinkedList[3];
-	public static String devCurrProc[][] = {{null,null,null}, {null,null}, {null}};
+	public static int devCurrPid[][] = {{-1,-1,-1}, {-1,-1}, {-1}};
 }
