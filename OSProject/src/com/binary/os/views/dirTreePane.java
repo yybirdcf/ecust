fcaea2b8ce91cb26823ce4b804731392ea35f17f
Administrator <Administrator@169.254.7.185>
2012-06-13 22:27:47 +0800

    目录树 Panel

---
+++ OSProject/src/com/binary/os/views/dirTreePane.java
@@ -0,0 +1,16 @@
+package com.binary.os.views;
+
+import javax.swing.JPanel;
+
+public class dirTreePane extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public dirTreePane() {
+		setBounds(10, 54, 212, 666);
+		setOpaque(false);
+		setLayout(null);
+	}
+
+}
